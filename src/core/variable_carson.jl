701887cc47d87d8fa146e4b3c0e5ff4b15c055b4
MartaVanin <marta.vanin@esat.kuleuven.be>
2024-03-28 16:13:51 +1000

    tidier variable definitions

--- src/core/variable_carson.jl
+++ src/core/variable_carson.jl
@@ -39,12 +39,12 @@ function variable_cross_section_and_gmr(pm::_PMD.AbstractExplicitNeutralIVRModel
     oh_or_ug = if equal_crossection _PMD.ref(pm, 1, :settings)["oh_or_ug"] end
 
     if !equal_crossection
-        A_p, gmr = variable_different_cross_sections(pm, conn)
+        A_p, gmr = variable_different_cross_sections(pm, conn, nw)
     else
         if oh_or_ug == "oh" # overhead lines have same crosss. for all conductors
-            A_p, gmr = variable_all_cross_sections_equal(pm, conn)
+            A_p, gmr = variable_all_cross_sections_equal(pm, conn, nw)
         else # underground cables have same crosss. for all phase cond, neutral can be up to 50% smaller tho
-            A_p, gmr = variable_phase_cross_sections_equal(pm, conn)
+            A_p, gmr = variable_phase_cross_sections_equal(pm, conn, nw)
         end
     end
 
@@ -71,30 +71,31 @@ end
 For every line code, defines one variable for A_p and one for gmr for every conductor.
 Possible future work is to make this more generic and allow some linecodes to have this or not and some viceversa.
 """
-function variable_different_cross_sections(pm::_PMD.AbstractExplicitNeutralIVRModel, conn::Dict)
-        A_p = _PMD.var(pm, nw)[:A_p] = Dict(i => JuMP.@variable(pm.model,
-                    [t in conn[i]],
-                    base_name="A_p_$(i)",
-                    lower_bound = 10, 
-                    upper_bound = 300, 
-                    start = _PMD.comp_start_value(linecode, "A_p_start", t, 20.) 
-                    ) for (i, linecode) in _PMD.ref(pm, 1, :linecode_map) 
-                ) 
+function variable_different_cross_sections(pm::_PMD.AbstractExplicitNeutralIVRModel, conn::Dict, nw::Int)
 
-        gmr = _PMD.var(pm, nw)[:gmr] = Dict(i => JuMP.@variable(pm.model,
-            [t in conn[i]],
-            base_name="gmr_$(i)",
-            lower_bound = .7, 
-            upper_bound = 9., 
-            start = _PMD.comp_start_value(linecode, "gmr_start", t, 1.965) 
-            ) for (i, linecode) in _PMD.ref(pm, 1, :linecode_map) 
-        ) 
+    A_p = _PMD.var(pm, nw)[:A_p] = Dict(i => JuMP.@variable(pm.model,
+                [t in conn[i]],
+                base_name="A_p_$(i)",
+                lower_bound = 10, 
+                upper_bound = 300, 
+                start = _PMD.comp_start_value(linecode, "A_p_start", t, 20.) 
+                ) for (i, linecode) in _PMD.ref(pm, 1, :linecode_map) 
+            ) 
+
+    gmr = _PMD.var(pm, nw)[:gmr] = Dict(i => JuMP.@variable(pm.model,
+        [t in conn[i]],
+        base_name="gmr_$(i)",
+        lower_bound = .7, 
+        upper_bound = 9., 
+        start = _PMD.comp_start_value(linecode, "gmr_start", t, 1.965) 
+        ) for (i, linecode) in _PMD.ref(pm, 1, :linecode_map) 
+    ) 
     return A_p, gmr
 end
 """
 All conductors have the same cross section. All means phase conductors as well as the neutral.
 """
-function variable_all_cross_sections_equal(pm::_PMD.AbstractExplicitNeutralIVRModel, conn::Dict)
+function variable_all_cross_sections_equal(pm::_PMD.AbstractExplicitNeutralIVRModel, conn::Dict, nw::Int)
     A_all = Dict(i => JuMP.@variable(pm.model,
                         base_name="A_ph_$(i)",
                         lower_bound = 12, 
@@ -122,7 +123,8 @@ end
 """
 All phase conductors have the same cross section, but the neutral can be up to 50% smaller.
 """
-function variable_phase_cross_sections_equal(pm::_PMD.AbstractExplicitNeutralIVRModel, conn::Dict)
+function variable_phase_cross_sections_equal(pm::_PMD.AbstractExplicitNeutralIVRModel, conn::Dict, nw::Int)
+
     A_ph = Dict(i => JuMP.@variable(pm.model,
                       base_name="A_ph_$(i)",
                       lower_bound = 12, 
@@ -169,138 +171,30 @@ function variable_coordinates_and_distances(pm::_PMD.AbstractExplicitNeutralIVRM
 
     nw = 1 # variable only defined at the first timestep
 
-    two_w   = [ i for (i,l) in _PMD.ref(pm, nw, :linecode_map) if l["n_wires"] == 2 ]
-    three_w = [ i for (i,l) in _PMD.ref(pm, nw, :linecode_map) if l["n_wires"] == 3 ]
-    four_w  = [ i for (i,l) in _PMD.ref(pm, nw, :linecode_map) if l["n_wires"] == 4 ]
+    dij_2w, report_idx_2w_dij = variable_distances_two_wire_segments(pm, nw)
+    dij_3w, report_idx_3w_dij = variable_distances_three_wire_segments(pm, nw)
 
-    dij_2w = Dict(i => JuMP.@variable(pm.model,
-        [1:1], # making it a vector for consistency with the rest (access through indices later on)
-        base_name="dij_2w_code_$i",
-        lower_bound = 2, 
-        upper_bound = 100, 
-        ) for (i,l) in _PMD.ref(pm, 1, :linecode_map) if i ∈ two_w
-    ) 
-
-    distance_indices = [(1,2), (1,3), (2,3), (1,4), (2,4), (3,4)] # order of entries of distance matrix elements
-
-    dij_3w = Dict(i => JuMP.@variable(pm.model,
-        [j = 1:3],
-        base_name="dij_3w_code_$(i)",
-        lower_bound = 1,
-        upper_bound =  4e3, 
-        ) for (i,l) in _PMD.ref(pm, 1, :linecode_map) if i ∈ three_w
-    )
-
-    exploit_horizontality = haskey(_PMD.ref(pm, 1, :settings), "exploit_horizontality") && _PMD.ref(pm, 1, :settings)["exploit_horizontality"]
-    exploit_squaredness   = haskey(_PMD.ref(pm, 1, :settings), "exploit_squaredness"  ) && _PMD.ref(pm, 1, :settings)["exploit_squaredness"]
-    @assert !(exploit_squaredness*exploit_horizontality) "Horizontality and squaredness are mutully exclusive, change one of the two to false"
+    exploit_horizontality = haskey(_PMD.ref(pm, nw, :settings), "exploit_horizontality") && _PMD.ref(pm, nw, :settings)["exploit_horizontality"]
+    exploit_squaredness   = haskey(_PMD.ref(pm, nw, :settings), "exploit_squaredness"  ) && _PMD.ref(pm, nw, :settings)["exploit_squaredness"]
+    @assert !(exploit_squaredness*exploit_horizontality) "Horizontality and squaredness are mutually exclusive, change one of the two to false"
 
     if exploit_horizontality
-
-        dij_4w = Dict(i => JuMP.@variable(pm.model,
-                [j = 1:3], # only three distances are needed! instead of 6
-                base_name="dij_4w_code_$(i)",
-                lower_bound = 380,
-                upper_bound = 1.2e3, # this is because the distances are  [(1,2), (2,3), (3,4)] so sum is never above 3meters
-                ) for (i,l) in _PMD.ref(pm, 1, :linecode_map) if i ∈ four_w
-        )
-
+        dij_4w = variable_distances_horizontal_case(pm, nw) # no coordinates are created, the only variables are the distances 
     elseif exploit_squaredness
-
-        bigD = Dict(i => JuMP.@variable(pm.model,
-                [j = 1:1], 
-                base_name="dij_4w_code_$(i)",
-                lower_bound = 10,
-                upper_bound = 60,
-                ) for (i,l) in _PMD.ref(pm, 1, :linecode_map) if i ∈ four_w
-        )
-
-        smallD = Dict(i => JuMP.@variable(pm.model,
-                [j = 1:1], 
-                base_name="dij_4w_code_$(i)",
-                lower_bound = 10,
-                upper_bound = 40,
-                ) for (i,l) in _PMD.ref(pm, 1, :linecode_map) if i ∈ four_w
-        )
-
-        dij_4w = _PMD.var(pm, nw)[:dij_4w] = Dict(i => vcat(bigD[i], smallD[i]) for (i, linecode) in _PMD.ref(pm, 1, :linecode_map) if i ∈ four_w) 
-
+        dij_4w = variable_distances_squared_case(pm, nw)
     else
-        
-        xcoord = _PMD.var(pm, nw)[:xcoord] = Dict(i => JuMP.@variable(pm.model,
-                    [j = 2:l["n_wires"]], # starts from 2 because first cond is (0,0)
-                    base_name="xcoord_code_$(i)",
-                    lower_bound = 1, 
-                    upper_bound =  3e3, 
-                    ) for (i,l) in _PMD.ref(pm, 1, :linecode_map) if i ∈ vcat(three_w, four_w)
-                )
-
-        for (i,_) in _PMD.ref(pm, 1, :linecode_map) 
-            if i ∈ vcat(three_w, four_w)
-                JuMP.@constraint(pm.model, xcoord[i][3]>=xcoord[i][2])
-                if i ∈ four_w
-                    JuMP.@constraint(pm.model, xcoord[i][4]>=xcoord[i][3])
-                end
-            end
-        end
-
-        ycoord = _PMD.var(pm, nw)[:ycoord] = Dict(i => JuMP.@variable(pm.model,
-                    [j = 2:l["n_wires"]], # starts from 2 because first cond is (0,0)
-                    base_name="ycoord_code_$(i)",
-                    lower_bound = -2e3, 
-                    upper_bound =  0, 
-                ) for (i,l) in _PMD.ref(pm, 1, :linecode_map) if i ∈ vcat(three_w, four_w)
-            )
-
-        dij_4w = Dict(i => JuMP.@variable(pm.model,
-            [j = 1:6],
-            base_name="dij_4w_code_$(i)",
-            lower_bound = 1,
-            upper_bound =  3e3, 
-            ) for (i,l) in _PMD.ref(pm, 1, :linecode_map) if i ∈ four_w
-        )
-
-        for (i,l) in _PMD.ref(pm, nw, :linecode_map)
-            if i ∈ three_w
-                for (id, ij) in enumerate(distance_indices[1:3])
-                    x0 = ij[1] == 1 ? 0. : xcoord[i][ij[1]] # because the coordinates of the first conductor are fixed to zero
-                    y0 = ij[1] == 1 ? 0. : ycoord[i][ij[1]]
-                    JuMP.@constraint(pm.model, dij_3w[i][id]^2 == (x0-xcoord[i][ij[2]])^2+(y0-ycoord[i][ij[2]])^2 )
-                end
-            elseif i ∈ four_w
-                for (id, ij) in enumerate(distance_indices)
-                    x0 = ij[1] == 1 ? 0. : xcoord[i][ij[1]] # because the coordinates of the first conductor are fixed to zero
-                    y0 = ij[1] == 1 ? 0. : ycoord[i][ij[1]]
-                    JuMP.@constraint(pm.model, dij_4w[i][id]^2 == (x0-xcoord[i][ij[2]])^2+(y0-ycoord[i][ij[2]])^2 )
-                end
-            end
-        end
-
+        xcoord, ycoord, dij_4w = variable_generic_coord_dist_four_wire(pm, nw)
     end
 
     _PMD.var(pm, nw)[:dij] = merge(dij_2w, dij_3w, dij_4w)
 
     if bounded
-        for (i,linecode) in _PMD.ref(pm, 1, :linecode_map) 
-            conns = linecode["n_wires"]
-            if i ∈ vcat(three_w, four_w)
-                for idx in 1:conns-1 
-                    if haskey(linecode, "xcoord_max") JuMP.set_upper_bound(xcoord[i][idx], linecode["xcoord_max"][idx]) end
-                    if haskey(linecode, "xcoord_min") JuMP.set_lower_bound(xcoord[i][idx], linecode["xcoord_min"][idx]) end 
-                    if haskey(linecode, "ycoord_max") JuMP.set_upper_bound(ycoord[i][idx], linecode["ycoord_max"][idx]) end
-                    if haskey(linecode, "ycoord_min") JuMP.set_lower_bound(ycoord[i][idx], linecode["ycoord_min"][idx]) end 
-                end
-                if i ∈ vcat(four_w)
-                    @info "Please mind the `distance_indices` if you add bounds on the dij's"
-                    for idx in 1:4
-                        if haskey(linecode, "dij_4w_max") JuMP.set_upper_bound(dij_4w[i][idx], linecode["dij_4w_max"][idx]) end
-                        if haskey(linecode, "dij_4w_min") JuMP.set_lower_bound(dij_4w[i][idx], linecode["dij_4w_min"][idx]) end     
-                    end
-                else
-                    for idx in 1:3
-                        if haskey(linecode, "dij_3w_max") JuMP.set_upper_bound(dij_3w[i][idx], linecode["dij_3w_max"][idx]) end
-                        if haskey(linecode, "dij_3w_min") JuMP.set_lower_bound(dij_3w[i][idx], linecode["dij_3w_min"][idx]) end     
-                    end
+        for (i,linecode) in _PMD.ref(pm, 1, :linecode_map)            
+            if i ∈ four_w
+                @info "Please mind the `distance_indices` if you add bounds on the dij's"
+                for idx in 1:length(dij_4w[i])
+                    if haskey(linecode, "dij_4w_max") JuMP.set_upper_bound(dij_4w[i][idx], linecode["dij_4w_max"][idx]) end
+                    if haskey(linecode, "dij_4w_min") JuMP.set_lower_bound(dij_4w[i][idx], linecode["dij_4w_min"][idx]) end     
                 end
             else
                 if haskey(linecode, "dij_2w_max") JuMP.set_upper_bound(dij_2w[i][1], linecode["dij_2w_max"]) end
@@ -309,20 +203,144 @@ function variable_coordinates_and_distances(pm::_PMD.AbstractExplicitNeutralIVRM
         end
     end
 
-    report_idx_2w_dij = [i for (i,l) in _PMD.ref(pm, 1, :linecode_map) if i ∈ two_w]
-    report_idx_3w_dij = [i for (i,l) in _PMD.ref(pm, 1, :linecode_map) if i ∈ three_w]
-    report_idx_4w_dij = [i for (i,l) in _PMD.ref(pm, 1, :linecode_map) if i ∈ four_w]
-    report_idx_coords = [i for (i,l) in _PMD.ref(pm, 1, :linecode_map) if i ∈ vcat(three_w, four_w)]
+    report_idx_4w_dij = [i for (i,l) in _PMD.ref(pm, nw, :linecode_map) if i ∈ four_w]
+    report_idx_coords = [i for (i,l) in _PMD.ref(pm, nw, :linecode_map) if i ∈ four_w]
 
-    report && _IM.sol_component_value(pm, :pmd, 1, :linecode_map, :dij_2w, report_idx_2w_dij, dij_2w)
-    report && _IM.sol_component_value(pm, :pmd, 1, :linecode_map, :dij_3w, report_idx_3w_dij, dij_3w)
-    report && _IM.sol_component_value(pm, :pmd, 1, :linecode_map, :dij_4w, report_idx_4w_dij, dij_4w)
+    report && _IM.sol_component_value(pm, :pmd, nw, :linecode_map, :dij_2w, report_idx_2w_dij, dij_2w)
+    report && _IM.sol_component_value(pm, :pmd, nw, :linecode_map, :dij_3w, report_idx_3w_dij, dij_3w)
+    report && _IM.sol_component_value(pm, :pmd, nw, :linecode_map, :dij_4w, report_idx_4w_dij, dij_4w)
 
     if !exploit_horizontality && !exploit_squaredness
-        report && _IM.sol_component_value(pm, :pmd, 1, :linecode_map, :xcoord, report_idx_coords, xcoord)
-        report && _IM.sol_component_value(pm, :pmd, 1, :linecode_map, :ycoord, report_idx_coords, ycoord)
+        report && _IM.sol_component_value(pm, :pmd, nw, :linecode_map, :xcoord, report_idx_coords, xcoord)
+        report && _IM.sol_component_value(pm, :pmd, nw, :linecode_map, :ycoord, report_idx_coords, ycoord)
     end
 
+end
+
+function variable_generic_coord_dist_four_wire(pm, nw)
+    four_w  = [ i for (i,l) in _PMD.ref(pm, nw, :linecode_map) if l["n_wires"] == 4 ]
+    xcoord = _PMD.var(pm, nw)[:xcoord] = Dict(i => JuMP.@variable(pm.model,
+                [j = 2:l["n_wires"]], # starts from 2 because first cond is (0,0)
+                base_name="xcoord_code_$(i)",
+                lower_bound = 1, 
+                upper_bound =  3e3, 
+                ) for (i,l) in _PMD.ref(pm, nw, :linecode_map) if i ∈ four_w
+            )
+
+    for (i,_) in _PMD.ref(pm, 1, :linecode_map) 
+        if i ∈ four_w JuMP.@constraint(pm.model, xcoord[i][4]>=xcoord[i][3]) end
+    end
+
+    ycoord = _PMD.var(pm, nw)[:ycoord] = Dict(i => JuMP.@variable(pm.model,
+                [j = 2:l["n_wires"]], # starts from 2 because first cond is (0,0)
+                base_name="ycoord_code_$(i)",
+                lower_bound = -2e3, 
+                upper_bound =  0, 
+            ) for (i,l) in _PMD.ref(pm, nw, :linecode_map) if i ∈ four_w
+        )
+
+    dij_4w = Dict(i => JuMP.@variable(pm.model,
+        [j = 1:6],
+        base_name="dij_4w_code_$(i)",
+        lower_bound = 1,
+        upper_bound =  3e3, 
+        ) for (i,l) in _PMD.ref(pm, 1, :linecode_map) if i ∈ four_w
+    )
+
+    for (i,l) in _PMD.ref(pm, nw, :linecode_map)
+        if i ∈ four_w
+            for (id, ij) in enumerate(distance_indices())
+                x0 = ij[1] == 1 ? 0. : xcoord[i][ij[1]] # because the coordinates of the first conductor are fixed to zero
+                y0 = ij[1] == 1 ? 0. : ycoord[i][ij[1]]
+                JuMP.@constraint(pm.model, dij_4w[i][id]^2 == (x0-xcoord[i][ij[2]])^2+(y0-ycoord[i][ij[2]])^2 )
+            end
+        end
+    end
+    return xcoord, ycoord, dij_4w
+end
+
+distance_indices() = [(1,2), (1,3), (2,3), (1,4), (2,4), (3,4)] # order of entries of distance matrix elements, for reference
+
+function variable_distances_squared_case(pm, nw)
+    four_w  = [ i for (i,l) in _PMD.ref(pm, nw, :linecode_map) if l["n_wires"] == 4 ]
+    big_dist = Dict(i => JuMP.@variable(pm.model,
+        [j = 1:1], 
+        base_name="dij_4w_code_$(i)",
+        lower_bound = 10,
+        upper_bound = 60,
+        ) for (i,l) in _PMD.ref(pm, nw, :linecode_map) if i ∈ four_w
+    )
+
+    small_dist = Dict(i => JuMP.@variable(pm.model,
+        [j = 1:1], 
+        base_name="dij_4w_code_$(i)",
+        lower_bound = 10,
+        upper_bound = 40,
+        ) for (i,l) in _PMD.ref(pm, nw, :linecode_map) if i ∈ four_w
+    )
+
+    dij_4w = _PMD.var(pm, nw)[:dij_4w] = Dict(i => vcat(big_dist[i], small_dist[i]) for (i, linecode) in _PMD.ref(pm, nw, :linecode_map) if i ∈ four_w) 
+    return dij_4w
+end
+
+function variable_distances_squared_case_new(pm, nw)
+    
+    four_w  = [ i for (i,l) in _PMD.ref(pm, nw, :linecode_map) if l["n_wires"] == 4 ]
+
+    xcoord = _PMD.var(pm, nw)[:xcoord] = Dict(i => JuMP.@variable(pm.model,
+                [j = 1:2], 
+                base_name="xcoord_code_$(i)",
+                lower_bound = 1., 
+                upper_bound = 100., 
+                ) for (i,l) in _PMD.ref(pm, nw, :linecode_map) if i ∈ four_w
+            )
+
+    ycoord = _PMD.var(pm, nw)[:ycoord] = Dict(i => JuMP.@variable(pm.model,
+            [j = 1:2], 
+            base_name="ycoord_code_$(i)",
+            lower_bound = -100., 
+            upper_bound =  -1., 
+        ) for (i,l) in _PMD.ref(pm, nw, :linecode_map) if i ∈ four_w
+    )
+
+    return xcoord, ycoord
+end
+
+function variable_distances_horizontal_case(pm, nw)
+    four_w  = [ i for (i,l) in _PMD.ref(pm, nw, :linecode_map) if l["n_wires"] == 4 ]
+    return Dict(i => JuMP.@variable(pm.model,
+            [j = 1:3], # only three distances are needed instead of 6
+            base_name="dij_4w_code_$(i)",
+            lower_bound = 380,
+            upper_bound = 1.2e3, # this is because the distances are  [(1,2), (2,3), (3,4)] so sum is never above 3meters
+            ) for (i,l) in _PMD.ref(pm, nw, :linecode_map) if i ∈ four_w
+        )
+end
+
+function variable_distances_two_wire_segments(pm, nw::Int)
+    two_w   = [ i for (i,l) in _PMD.ref(pm, nw, :linecode_map) if l["n_wires"] == 2 ]
+    report_ids = [i for (i,l) in _PMD.ref(pm, 1, :linecode_map) if i ∈ two_w]
+    dij_2w = Dict(i => JuMP.@variable(pm.model,
+        [1:1], # making it a vector for consistency with the rest (access through indices later on)
+        base_name="dij_2w_code_$i",
+        lower_bound = 2, 
+        upper_bound = 100, 
+        ) for (i,l) in _PMD.ref(pm, 1, :linecode_map) if i ∈ two_w
+    ) 
+    return dij_2w, report_ids
+end
+
+function variable_distances_three_wire_segments(pm, nw::Int)
+    three_w = [ i for (i,l) in _PMD.ref(pm, nw, :linecode_map) if l["n_wires"] == 3 ]
+    report_ids = [i for (i,l) in _PMD.ref(pm, 1, :linecode_map) if i ∈ three_w]
+    dij_3w = Dict(i => JuMP.@variable(pm.model,
+        [j = 1:3],
+        base_name="dij_3w_code_$(i)",
+        lower_bound = 1,
+        upper_bound =  4e3, 
+        ) for (i,l) in _PMD.ref(pm, 1, :linecode_map) if i ∈ three_w
+    )
+    return dij_3w, report_ids
 end
 
 function variable_bus_shunt_admittance(pm::_PMD.AbstractExplicitNeutralIVRModel; bounded::Bool=true, report::Bool=true)
