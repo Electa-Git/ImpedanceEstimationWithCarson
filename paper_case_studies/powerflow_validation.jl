8c183774b98fc0fe190c09a15bbe4bf9ff36afaa
MartaVanin <marta.vanin@esat.kuleuven.be>
2024-07-17 09:44:47 +0200

    RM old results

--- paper_case_studies/powerflow_validation.jl
+++ paper_case_studies/powerflow_validation.jl
@@ -37,14 +37,14 @@ for folder in ["eulvtf_oh_cross_only", "eulvtf_oh_horizontal_only", "eulvtf_oh_m
                 end
             end
         else
-            # for power_mult in [1.0]#, 2.0]
-            #     extra_id = "_power_mult_$(power_mult)"
-            #     estimated_linecode = [f for f in readdir(joinpath(general_result_path, folder)) if occursin("_linecode_results_scenario_1__power_mult_$(power_mult)_", f)][1]
-            #     estimated_branch_length = [f for f in readdir(joinpath(general_result_path, folder)) if occursin("_length_dict_scenario_1__power_mult_$(power_mult)_", f)][1]
-            #     _estimated_shunts = [f for f in readdir(joinpath(general_result_path, folder)) if occursin("shunts_scenario_1__power_mult_$(power_mult)_", f)]
-            #     estimated_shunts = isempty(_estimated_shunts) ? [] : joinpath(general_result_path, folder,_estimated_shunts[1])
-            #     powerflow_validation(feeder_name, "oh", result_path, estimated_shunts, joinpath(general_result_path, folder,estimated_linecode), joinpath(general_result_path, folder,estimated_branch_length), profiles, extra_id, validation_timesteps, pf_solver; power_mult=power_mult)
-            # end
+            for power_mult in [1.0]#, 2.0]
+                extra_id = "_power_mult_$(power_mult)"
+                estimated_linecode = [f for f in readdir(joinpath(general_result_path, folder)) if occursin("_linecode_results_scenario_1__power_mult_$(power_mult)_", f)][1]
+                estimated_branch_length = [f for f in readdir(joinpath(general_result_path, folder)) if occursin("_length_dict_scenario_1__power_mult_$(power_mult)_", f)][1]
+                _estimated_shunts = [f for f in readdir(joinpath(general_result_path, folder)) if occursin("shunts_scenario_1__power_mult_$(power_mult)_", f)]
+                estimated_shunts = isempty(_estimated_shunts) ? [] : joinpath(general_result_path, folder,_estimated_shunts[1])
+                powerflow_validation(feeder_name, "oh", result_path, estimated_shunts, joinpath(general_result_path, folder,estimated_linecode), joinpath(general_result_path, folder,estimated_branch_length), profiles, extra_id, validation_timesteps, pf_solver; power_mult=power_mult)
+            end
         end
     end
 end
